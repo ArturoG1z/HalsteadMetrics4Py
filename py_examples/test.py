34dc7f33ad09c073bea23e85c47cc58d8e618bad
Rodolfo Martínez Anaya <rorromart@gmail.com>
2022-03-09 21:16:11 -0600

    Added step number 8 to algoritmo.md

--- py_examples/test.py
+++ py_examples/test.py
@@ -2,13 +2,13 @@ def mul(x,y):
   z = 0
 #  z = ' dsafas fasd fdsa = saf=da= ==af '
 #  y = '''
-  esto = " s i "
-  print(esto)
+  # esto = " s i "
+  # print(esto)
   while ( x > 0 ):# sis jsaljkfdla
     z = z + y
     x = x - 1
-    y = z == x
-    x **= 2
-    y *= 2
+    # y = z == x
+    # x **= 2
+    # y *= 2
   return z
-mul(2,3)
+# mul(2,3)
